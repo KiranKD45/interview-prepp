0c224edaf2c43b19ad590df14c492f365521afc9
kiran madiwalar <kiranm81040@gmail.com>
2023-07-23 20:02:17 +0530

    js array done

---
+++ JS/arrayMethods.js
@@ -0,0 +1,20 @@
+//pop,push,shift,unshift,slice and splice.
+
+let arr = [45, "Hitman", "kiran", "openers"];
+// arr.push("The greatest") //it adds the element to last
+// console.log(arr);
+// arr.pop() // it removes last element
+// console.log(arr.length);
+
+// arr.unshift("18")// adds the element to 0th index
+// arr.shift()// removes the element at 0th index
+// console.log(arr.slice(2,3));
+// arr.splice(3,1,"msd", "chiku","hitman from another univerce");//where   3 is from which index;
+      //1 is how many delete or replace
+      // at last what we have to add... 
+// console.log(arr);
+
+// arr.fill("Indian cricket team is best",2,3)
+// console.log(arr);
+
+
---
+++ JS/scopeChaining.js
@@ -0,0 +1,20 @@
+// let kiran = "he is greate man"
+// console.log(kiran); // kiran
+
+// console.log(kiran); //reference error ==> cannot access before initialization
+// let kiran = "he is greate man"
+
+
+// console.log(hitesh); //reference error ==> cannot access before initialization
+// const hitesh = "he is a best teacher"
+
+
+//the var declaration are scanned and made undefined.
+// console.log(kiran); //undefined
+// var kiran = "lets try"
+
+//the function declarations are scanned and made available ***
+// kiran();
+// function kiran(){
+//     console.log("hello");
+// }
