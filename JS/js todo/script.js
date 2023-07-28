fc6d79fe44966aefbaace6484534881167a4cc09
kiran madiwalar <kiranm81040@gmail.com>
2023-07-28 11:34:38 +0530

    to do doing

---
+++ JS/js todo/script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input")
+const main = document.getElementById("main")
+
+function addTask(){
+    if (input.value == "") {
+        alert("please enter a todo")
+    }
+    else{
+        const li = document.createElement("li")
+        li.innerHTML = input.value;
+        main.appendChild(li);
+
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    input.value == "";
+}
+
+main.addEventListener("click", (e)=>{
+if(e.target.value === "LI"){
+    e.target.classList.toggle("ckecked")
+}
+else if(e.target.value === "SPAN"){
+    e.target.parentElement.remove();
+
+}
+})
