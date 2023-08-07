913d42910115a98ba78260702e86f04831014012
kiran madiwalar <kiranm81040@gmail.com>
2023-08-07 20:08:07 +0530

    updated

--- JS/promise.js
+++ JS/promise.js
@@ -1,25 +1,25 @@
-// // let promise = new Promise((resolve, reject)=>{
-// //    reject("learning promise");
-// // })
-// // promise.then(()=>{
-// // console.log("resolved", promise);
-// // })
-// // promise.catch(()=>{
-// //     console.log(promise);
-// // })
+// // let promise = new Promise((resolve, reject) => {
+// //   reject("learning promise");
+// // });
+// // // // promise.then(()=>{
+// // // // console.log("resolved", promise);
+// // // // })
+// // // // promise.catch(()=>{
+// // // //     console.log(promise);
+// // // // })
 
-// let bigpromise = new Promise ((resolve, reject)=>{
-//     setTimeout(()=>{
-//         resolve("afte 2 sec ")
-//     }, 2000);
-    
-//     setTimeout(()=>{
-//         resolve("after 3 sec")
-//     }, 3000);
-//     setTimeout(()=>{
-//         resolve("after 4 sec")
-//     },4000);
+// let bigpromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("afte 2 sec ");
+//   }, 2000);
 
-// })
+//   setTimeout(() => {
+//     resolve("after 3 sec");
+//   }, 3000);
+//   setTimeout(() => {
+//     resolve("after 4 sec");
+//   }, 4000);
+// });
 
-// Promise.all(bigpromise)
+// console.log(bigpromise);
+// // Promise.all(bigpromise);
