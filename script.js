7fde9261b77b3c71404be0e36c31240cefe65494
kiran madiwalar <kiranm81040@gmail.com>
2023-08-15 11:46:10 +0530

    learning

---
+++ script.js
@@ -0,0 +1,16 @@
+let array = [10, 02, 20, 45, 15];
+// console.log(Math.max(...array));
+
+// console.log(
+//   array.map((e, index) => {
+//     return e * index;
+//   })
+// );
+// array.filter((num) => {
+//   console.log(num > 10);
+// });
+
+// console.log(array.fill(46, 4));
+
+// console.log(array.indexOf(45));
+// console.log(array);
